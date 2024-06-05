56ff4f2263c13eeb71e1e0ed29a04a0c1f68656b
Jehad Cemanes <jehadcemanesece@gmail.com>
2024-06-06 01:10:12 +0800

    Create function for get disk information and generate log, Improve code comments for clarity and consistency

--- DiskSpaceMonitoring/DiskSpaceMonitoring.ps1
+++ DiskSpaceMonitoring/DiskSpaceMonitoring.ps1
@@ -1,44 +1,77 @@
 param(
-    [string]$LogPath="$HOME\workspace\DiskSpace.txt",
-    [string]$Threshold="25"
+    [int]$FreeSpacePercentageThreshold = "25",                         # Default free space percentage threshold
+    [string]$LogPath = "$HOME\workspace\logs\DiskSpaceInformation.log" # Default log path
 )
 
-function Check-DiskSpace {
+# Function to retrieve disk information
+function Get-DiskInformation {
     param(
-        [Parameter(Mandatory=$true)]
-        [string]$LogPath,
-        [Parameter(Mandatory=$true)]
-        [string]$Threshold
+        [int]$FreeSpacePercentageThreshold # free space percentage threshold
     )
 
-    $logContent = @()
-    $logDir = Split-Path -Path $LogPath -Parent
+    try {
+        # Retrieve disk information
+        $drives = Get-PSDrive -PSProvider FileSystem
 
-    New-Item -Path $logDir -ItemType Directory -Force | Out-Null
-    Remove-Item -Path $LogPath -Confirm:$false -ErrorAction SilentlyContinue
-
-    $drives = Get-PSDrive -PSProvider FileSystem
-    foreach ($drive in $drives) {
-        $freeSpace = [Math]::Round($drive.Free / 1GB, 2)
-        $usedSpace = [Math]::Round($drive.Used / 1GB, 2)
-        $totalSpace = $drive.Used + $drive.Free
-        $freeSpacePercentage = [Math]::Round($drive.Free / $totalSpace, 4) * 100
-        $status = "OK"
-        if ($freeSpacePercentage -lt $Threshold) {
-            $status = "NOT OK"
+        $diskInformation = @()
+        foreach ($drive in $drives) {
+            $freeSpace           = [Math]::Round($drive.Free / 1GB, 2)
+            $usedSpace           = [Math]::Round($drive.Used / 1GB, 2)
+            $totalSpace          = $drive.Used + $drive.Free
+            $freeSpacePercentage = [Math]::Round($drive.Free / $totalSpace, 4) * 100
+            $status              = if ($freeSpacePercentage -lt $FreeSpacePercentageThreshold) { "PASSED" } else { "FAILED" }
+            $diskInformation += [PSCustomObject]@{
+                Drive               = $drive.Name
+                FreeSpace           = $freeSpace
+                UsedSpace           = $usedSpace
+                FreeSpacePercentage = $freeSpacePercentage
+                Status              = $status
+            }
         }
 
-        $diskInformation = [PSCustomObject]@{
-            Drive = $drive.Name
-            FreeSpace = "$freeSpace ($freeSpacePercentage%)"
-            Status = $status
-        }
-
-        $logContent += $diskInformation
+        return $diskInformation
+    } catch {
+        # Handle potential errors during disk information retrieval
+        Write-Host 'Failed to retrieved disk information'
+        return $null
     }
-
-    $logContent | Format-Table -AutoSize | Out-String | Out-File -FilePath $LogPath -Append
-    Write-Output "Successfully logged disk information"
 }
 
-Check-DiskSpace -LogPath $LogPath -Threshold $Threshold
+# Function to generate a log file with disk information
+function Generate-Log {
+    param(
+        [PSCustomObject]$DiskInformation,      # Disk information object
+        [string]$LogPath                       # Path to save the log file
+    )
+
+    $logDirectory = Split-Path -Path $LogPath -Parent
+
+    # Ensure log directory exists, Remove existing log file if it exists
+    New-Item -Path $logDirectory -ItemType Directory -Force | Out-Null
+    if (Test-Path -Path $LogPath) {
+        Remove-Item -Path $LogPath -Confirm:$false
+    }
+
+    # Prepare log content
+    $logContent = @()
+    $logContent += "********************"
+    $logContent += "Disk Information"
+    $logContent += "********************"
+    $logContent += $DiskInformation | Format-Table -AutoSize
+
+    try {
+        # Write log content to file
+        $logContent | Out-File -FilePath $LogPath
+        Write-Host 'Successfully logged disk information'
+    } catch {
+        # Handle potential errors during disk information logging
+        Write-Host 'Failed to logged disk information'
+    }    
+}
+
+# Main script execution
+# Retrieve disk information and generate log file if successful
+$diskInformation = Get-DiskInformation -FreeSpacePercentageThreshold $FreeSpacePercentageThreshold
+if ($diskInformation) {
+    Generate-Log -DiskInformation $diskInformation -LogPath $LogPath
+}
